0cb86ace9a6f7fbe1e39287a26af7010aae48517
Dhruv Shah <dhruvshah2811ds@gmail.com>
2025-03-24 14:30:28 -0400

    logic part done

--- scripts/script.js
+++ scripts/script.js
@@ -19,9 +19,24 @@ window.onload = function(){
         let sign_up = formhandler.terms;
 
         const name_regex = /^[A-Za-z]+$/;
-        const email_regex = /^[A-Za-z]+@[A-Za-z]+\.[a-zA-Z]{2,}$/;
+        const email_regex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+        const pass_regex = /^(?=.*[\W_]).{8,}$/;
 
         let validation = false;
+
+        // function togglePassword(id) {
+        //     alert("oo paji");
+        //     const passwordInput = document.getElementById(id);
+        //     const toggleBtn = document.querySelector(".toggle-pass");
+    
+        //     if (passwordInput.type === "password") {
+        //         passwordInput.type = "text";
+        //         toggleBtn.textContent = "🙈"; // Change to hide icon
+        //     } else {
+        //         passwordInput.type = "password";
+        //         toggleBtn.textContent = "👁️"; // Change back to eye icon
+        //     }
+        // }
 
         // onsubmit function
         formhandler.onsubmit = (e) => {
@@ -36,7 +51,7 @@ window.onload = function(){
                 lang: lang.value,
                 country: country.value,
                 postal: postal.value,
-                sign_up: sign_up.value,
+                sign_up: sign_up.checked,
             }
 
             console.log(userData);
@@ -45,27 +60,117 @@ window.onload = function(){
             console.log(localStorage.getItem("user")); 
 
             if(email.value === "" || !email_regex.test(email.value)){
-                email.style.borderColor = "red"
+                email.style.borderColor = "red";
                 email.focus();
+                errorSection.style.display = "block";
+                error.innerHTML = "Please Enter a Valid Email";
                 validation = false;
                 return false;
             }else{
                 email.style.borderColor = "revert"
+                errorSection.style.display = "none";
                 validation = true;
             }
+
+            if(pass.value === "" || !pass_regex.test(pass.value)){
+                pass.style.borderColor = "red";
+                pass.focus();
+                errorSection.style.display = "block";
+                error.innerHTML = "<ul><span>Password must include</span><li>8 characters</li><li>At least one special character</li></ul>";
+                validation = false;
+                return false;
+            }else{
+                pass.style.borderColor = "revert"
+                errorSection.style.display = "none";
+                validation = true;
+            }
+
+            if(conf_pass.value !== pass.value){
+                conf_pass.style.borderColor = "red";
+                conf_pass.focus();
+                errorSection.style.display = "block";
+                error.innerHTML = "Passwords Dont Match";
+                validation = false;
+                return false;
+            }else{
+                conf_pass.style.borderColor = "revert"
+                errorSection.style.display = "none";
+                validation = true;
+            }
+
+            if(fname.value === "" || !name_regex.test(fname.value)){
+                fname.style.borderColor = "red";
+                fname.focus();
+                errorSection.style.display = "block";
+                error.innerHTML = "Please Enter a Valid First Name";
+                validation = false;
+                return false;
+            }else{
+                fname.style.borderColor = "revert"
+                errorSection.style.display = "none";
+                validation = true;
+            }
+
+            if(lname.value === "" || !name_regex.test(lname.value)){
+                lname.style.borderColor = "red";
+                lname.focus();
+                errorSection.style.display = "block";
+                error.innerHTML = "Please Enter a Valid First Name";
+                validation = false;
+                return false;
+            }else{
+                lname.style.borderColor = "revert"
+                errorSection.style.display = "none";
+                validation = true;
+            }
+
+            if(sign_up.checked === false){
+                sign_up.style.borderColor = "red";
+                sign_up.focus();
+                errorSection.style.display = "block";
+                error.innerHTML = "You need to confirm the Terms and Conditions and Privacy Policies";
+                validation = false;
+                return false;
+            }else{
+                sign_up.style.borderColor = "revert"
+                errorSection.style.display = "none";
+                validation = true;
+            }
+            
         }
 
     }
 
     if(login_formhandler){
+        // Declarations
         let log_email = login_formhandler.log_email;
-        let log_pass = login_formhandler.log_email;
+        let log_pass = login_formhandler.log_pass;
+        
+        const errorSection = document.querySelector("#err_sec");
+        const error = document.querySelector("#error_text");
 
         let user_data = JSON.parse(localStorage.getItem("user"));
         log_email.value = user_data.email;
+        let stored_pass =  user_data.pass;
 
-        let user_pass = user_data.pass;
+        let validation = false;
 
+        login_formhandler.onsubmit = function(){
+
+
+            if(log_pass.value !== stored_pass){
+                log_pass.style.borderColor = "red";
+                log_pass.focus();
+                errorSection.style.display = "block";
+                error.innerHTML = "Incorrect Password";
+                validation = false;
+                return false;
+            }else{
+                log_pass.style.borderColor = "revert"
+                errorSection.style.display = "none";
+                validation = true;
+            }
+        }
     }
 
 }
